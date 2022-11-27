76db1bd84af51c8901d63e9394ce54ec27e6ce10
jbenegassi <119247943+jbenegassi@users.noreply.github.com>
2022-11-27 16:35:50 -0600

    Preprocessing work with Europe cases
    Preparing the data using Europe cases. Correlation value obtained between index and death cases is -0.79

---
+++ preprocessing_eur.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+us = pd.read_csv(r"C:\Users\ferna\OneDrive - Universidad Politécnica de Madrid\IIT\Machine Learning\Project\ProjectCSV\SP500&us_covid.csv", index_col=0)
+eur = pd.read_csv(r"C:\Users\ferna\OneDrive - Universidad Politécnica de Madrid\IIT\Machine Learning\Project\ProjectCSV\europe_cases.csv")
+
+us.rename(columns={"Deaths":"us_deaths"}, inplace=True)
+eur.rename(columns={"date":"Date", "new":"eur_deaths"}, inplace=True)
+eur.drop(columns=["deaths"], inplace=True)
+
+df = us.merge(eur, on="Date")
+df.set_index("Date", inplace=True)
+df.sort_index(inplace=True)
+
+# We need to normalize both values
+# We reduce up to the first 180 days
+# Without Weekends
+df.drop(columns=["Cases", "us_deaths"], inplace=True)
+df = df[:180]
+scaler = MinMaxScaler()
+scaler.fit(df)
+scaled = scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled, columns=df.columns)
+
+# Plot both Price and Deaths after normalization
+plt.plot(scaled_df["Price"], color="b")
+plt.plot(scaled_df["eur_deaths"], color="r")
+plt.show()
+
+correlation_matrix = df.corr()
+sns.heatmap(correlation_matrix, annot=True)
+
+
+
+
+
+
+
